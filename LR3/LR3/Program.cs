6c35ce1b2fa9020f8302ba8fee7d7d033b304173
ShaQuell <90714706+ShaQuell@users.noreply.github.com>
2021-11-30 00:44:18 +0200

    Add files via upload

--- LR3/LR3/Program.cs
+++ LR3/LR3/Program.cs
@@ -10,29 +10,30 @@ namespace LR3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Це калькулятор. Введіть перше число, бажану операцію та друге число у такому форматі: 1 + 2.\nБудь ласка, ставте пробіли між елементами \nОперації: +: додавання, -: віднімання, *: множення");
-            string s1 = Console.ReadLine();
-            string s2 = Console.ReadLine();
-            string s3 = Console.ReadLine();
-            String[] expressions = { s1, s2, s3 };
-            String pattern = @"(\d+)\s+([-+*])\s+(\d+)";
-            foreach (var expression in expressions) foreach (System.Text.RegularExpressions.Match m in System.Text.RegularExpressions.Regex.Matches(expression, pattern)) 
+            Console.WriteLine("Це калькулятор. Введіть перше число, бажану операцію та друге число у такому форматі: 15 + 28.\nБудь ласка, ставте пробіли між елементами \nОперації: +: додавання, -: віднімання, *: множення");
+            while (true) 
             {
-                    int value1 = Int32.Parse(m.Groups[1].Value);
-                    int value2 = Int32.Parse(m.Groups[3].Value);
-                    switch (m.Groups[2].Value)
+                string s1 = Console.ReadLine();
+                String[] expressions = { s1 };
+                String pattern = @"(\d+)\s+([-+*])\s+(\d+)";
+                foreach (var expression in expressions) foreach (System.Text.RegularExpressions.Match m in System.Text.RegularExpressions.Regex.Matches(expression, pattern))
                     {
-                        case "+":
-                            Console.WriteLine("{0} = {1}", m.Value, value1 + value2);
-                            break;
-                        case "-":
-                            Console.WriteLine("{0} = {1}", m.Value, value1 - value2);
-                            break;
-                        case "*":
-                            Console.WriteLine("{0} = {1}", m.Value, value1 * value2);
-                            break;
+                        int value1 = Int32.Parse(m.Groups[1].Value);
+                        int value2 = Int32.Parse(m.Groups[3].Value);
+                        switch (m.Groups[2].Value)
+                        {
+                            case "+":
+                                Console.WriteLine("{0} = {1}", m.Value, value1 + value2);
+                                break;
+                            case "-":
+                                Console.WriteLine("{0} = {1}", m.Value, value1 - value2);
+                                break;
+                            case "*":
+                                Console.WriteLine("{0} = {1}", m.Value, value1 * value2);
+                                break;
+                        }
                     }
-            } 
+            }
         }
     }
 }
